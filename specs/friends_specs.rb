bffb3c208c2c22afd0f5722591c09b63190d868a
DeepCFoxXx <learncode23@gmail.com>
2018-03-22 13:51:21 +0000

    test name passing

---
+++ specs/friends_specs.rb
@@ -0,0 +1,73 @@
+require( 'minitest/autorun' )
+require_relative( '../friends' )
+
+class TestFriends < MiniTest::Test
+
+  def setup
+
+    @person1 = {
+      name: "Rick",
+      age: 12,
+      monies: 1,
+      friends: ["Jay","Keith","Dave", "Val"],
+      favourites: {
+        tv_show: "Friends",
+        things_to_eat: ["charcuterie"]
+      }
+    }
+
+    @person2 = {
+      name: "Jay",
+      age: 15,
+      monies: 2,
+      friends: ["Keith"],
+      favourites: {
+        tv_show: "Baywatch",
+        things_to_eat: ["soup","bread"]
+      }
+    }
+
+    @person3 = {
+      name: "Val",
+      age: 18,
+      monies: 20,
+      friends: ["Rick", "Jay"],
+      favourites: {
+        tv_show: "Pokemon",
+        things_to_eat: ["ratatouille", "stew"]
+      }
+    }
+
+    @person4 = {
+      name: "Keith",
+      age: 18,
+      monies: 20,
+      friends: ["Rick", "Jay", "Dave"],
+      favourites: {
+        tv_show: "Pokemon",
+        things_to_eat: ["spaghetti"]
+      }
+    }
+
+    @person5 = {
+      name: "Dave",
+      age: 20,
+      monies: 100,
+      friends: [],
+      favourites: {
+        tv_show: "Scrubs",
+        things_to_eat: ["spinach"]
+      }
+    }
+
+    @people = [@person1, @person2, @person3, @person4, @person5]
+
+  end
+
+  # 1. For a given person, return their name
+  def test_getting_name
+    result = get_name(@person5)
+    assert_equal("Dave", result)
+  end
+
+end 
